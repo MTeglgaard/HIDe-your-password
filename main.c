f16cce4a697d789c1a50f65c4ba612e364f1b447
Michael Teglgaard <46675426+MTeglgaard@users.noreply.github.com>
2019-01-23 17:30:43 +0100

    Add files via upload
    Testing the first digit for all letters and numbers with and without shift. 
    Saving the time for Authenticater start to test the passswords and end test the passswords.
    Finding the largest time value. (NOT testet )

--- main.c
+++ main.c
@@ -9,10 +9,11 @@
 #define TICKS_WAIT 200
 /* Systick */
 void SysTick_Handler(void);
+uint8_t FindMax(int);
 
 uint32_t timeMeasured[12];
-uint32_t timeNumOn[24];
-uint32_t timeNumOff[24];
+uint32_t timeNumOn[96];
+uint32_t timeNumOff[96];
 uint32_t timeCapsOn[24];
 uint32_t timeCapsOff[24];
 static uint8_t indexNumOn =0;
@@ -21,6 +22,109 @@ static uint8_t indexCapsOn =0;
 static uint8_t indexCapsOff =0;
 static uint32_t ticks = 0;
 
+static uint8_t stringAlphabet[49]={
+GERMAN_KEYBOARD_SC_A,
+GERMAN_KEYBOARD_SC_B,
+GERMAN_KEYBOARD_SC_C,
+GERMAN_KEYBOARD_SC_D,
+GERMAN_KEYBOARD_SC_E,
+GERMAN_KEYBOARD_SC_F,
+GERMAN_KEYBOARD_SC_G,
+GERMAN_KEYBOARD_SC_H,
+GERMAN_KEYBOARD_SC_I,
+GERMAN_KEYBOARD_SC_J,
+GERMAN_KEYBOARD_SC_K,
+GERMAN_KEYBOARD_SC_L,
+GERMAN_KEYBOARD_SC_M,
+GERMAN_KEYBOARD_SC_N,
+GERMAN_KEYBOARD_SC_O,
+GERMAN_KEYBOARD_SC_P,
+GERMAN_KEYBOARD_SC_Q,
+GERMAN_KEYBOARD_SC_R,
+GERMAN_KEYBOARD_SC_S,
+GERMAN_KEYBOARD_SC_T,
+GERMAN_KEYBOARD_SC_U,
+GERMAN_KEYBOARD_SC_V,
+GERMAN_KEYBOARD_SC_W,
+GERMAN_KEYBOARD_SC_X,
+GERMAN_KEYBOARD_SC_Y,
+GERMAN_KEYBOARD_SC_Z,
+GERMAN_KEYBOARD_SC_1_AND_EXCLAMATION,
+GERMAN_KEYBOARD_SC_2_AND_QUOTES,
+GERMAN_KEYBOARD_SC_3_AND_PARAGRAPH,
+GERMAN_KEYBOARD_SC_4_AND_DOLLAR,
+GERMAN_KEYBOARD_SC_5_AND_PERCENTAGE,
+GERMAN_KEYBOARD_SC_6_AND_AMPERSAND,
+GERMAN_KEYBOARD_SC_7_AND_SLASH_AND_OPENING_BRACE,
+GERMAN_KEYBOARD_SC_8_AND_OPENING_PARENTHESIS_AND_OPENING_BRACKET,
+GERMAN_KEYBOARD_SC_9_AND_CLOSING_PARENTHESIS_AND_CLOSING_BRACKET,
+GERMAN_KEYBOARD_SC_0_AND_EQUAL_AND_CLOSING_BRACE,
+GERMAN_KEYBOARD_SC_SHARP_S_AND_QUESTION_AND_BACKSLASH,
+GERMAN_KEYBOARD_SC_CARET_AND_DEGREE,
+GERMAN_KEYBOARD_SC_PLUS_AND_ASTERISK_AND_TILDE ,
+GERMAN_KEYBOARD_SC_MINUS_AND_UNDERSCORE,
+GERMAN_KEYBOARD_SC_COMMA_AND_SEMICOLON,
+GERMAN_KEYBOARD_SC_DOT_AND_COLON,
+//GERMAN_KEYBOARD_SC_ENTER,
+//GERMAN_KEYBOARD_SC_ESCAPE,
+//GERMAN_KEYBOARD_SC_BACKSPACE,
+//GERMAN_KEYBOARD_SC_TAB,
+//GERMAN_KEYBOARD_SC_SPACE,
+//GERMAN_KEYBOARD_SC_HASHMARK_AND_APOSTROPHE,
+GERMAN_KEYBOARD_SC_LESS_THAN_AND_GREATER_THAN_AND_PIPE
+};
+
+static uint8_t stringShiftChar[49]={
+GERMAN_KEYBOARD_SC_A,
+GERMAN_KEYBOARD_SC_B,
+GERMAN_KEYBOARD_SC_C,
+GERMAN_KEYBOARD_SC_D,
+GERMAN_KEYBOARD_SC_E,
+GERMAN_KEYBOARD_SC_F,
+GERMAN_KEYBOARD_SC_G,
+GERMAN_KEYBOARD_SC_H,
+GERMAN_KEYBOARD_SC_I,
+GERMAN_KEYBOARD_SC_J,
+GERMAN_KEYBOARD_SC_K,
+GERMAN_KEYBOARD_SC_L,
+GERMAN_KEYBOARD_SC_M,
+GERMAN_KEYBOARD_SC_N,
+GERMAN_KEYBOARD_SC_O,
+GERMAN_KEYBOARD_SC_P,
+GERMAN_KEYBOARD_SC_Q,
+GERMAN_KEYBOARD_SC_R,
+GERMAN_KEYBOARD_SC_S,
+GERMAN_KEYBOARD_SC_T,
+GERMAN_KEYBOARD_SC_U,
+GERMAN_KEYBOARD_SC_V,
+GERMAN_KEYBOARD_SC_W,
+GERMAN_KEYBOARD_SC_X,
+GERMAN_KEYBOARD_SC_Y,
+GERMAN_KEYBOARD_SC_Z,
+GERMAN_KEYBOARD_SC_1_AND_EXCLAMATION,
+GERMAN_KEYBOARD_SC_2_AND_QUOTES,
+//GERMAN_KEYBOARD_SC_3_AND_PARAGRAPH,
+GERMAN_KEYBOARD_SC_4_AND_DOLLAR,
+//GERMAN_KEYBOARD_SC_5_AND_PERCENTAGE,
+GERMAN_KEYBOARD_SC_6_AND_AMPERSAND,
+GERMAN_KEYBOARD_SC_7_AND_SLASH_AND_OPENING_BRACE,
+GERMAN_KEYBOARD_SC_8_AND_OPENING_PARENTHESIS_AND_OPENING_BRACKET,
+GERMAN_KEYBOARD_SC_9_AND_CLOSING_PARENTHESIS_AND_CLOSING_BRACKET,
+GERMAN_KEYBOARD_SC_0_AND_EQUAL_AND_CLOSING_BRACE,
+GERMAN_KEYBOARD_SC_SHARP_S_AND_QUESTION_AND_BACKSLASH,
+//GERMAN_KEYBOARD_SC_CARET_AND_DEGREE,//^
+//GERMAN_KEYBOARD_SC_PLUS_AND_ASTERISK_AND_TILDE,
+GERMAN_KEYBOARD_SC_MINUS_AND_UNDERSCORE,
+GERMAN_KEYBOARD_SC_COMMA_AND_SEMICOLON,
+GERMAN_KEYBOARD_SC_DOT_AND_COLON,
+//GERMAN_KEYBOARD_SC_ENTER,
+//GERMAN_KEYBOARD_SC_ESCAPE,
+//GERMAN_KEYBOARD_SC_BACKSPACE,
+//GERMAN_KEYBOARD_SC_TAB,
+//GERMAN_KEYBOARD_SC_SPACE,
+//GERMAN_KEYBOARD_SC_HASHMARK_AND_APOSTROPHE,
+GERMAN_KEYBOARD_SC_LESS_THAN_AND_GREATER_THAN_AND_PIPE
+};
 /* Clock configuration */
 XMC_SCU_CLOCK_CONFIG_t clock_config = {
 	.syspll_config.p_div  = 2,
@@ -87,25 +191,25 @@ bool CALLBACK_HID_Device_CreateHIDReport(
 	USB_KeyboardReport_Data_t* report = (USB_KeyboardReport_Data_t *)ReportData;
 	*ReportSize = sizeof(USB_KeyboardReport_Data_t);
 	static uint8_t characterSent = 0,
-				   indexToSend = 0;
+				   indexToSend = 0,
+					 IndexAlphabet =0,
+					 indexMod=0;
+
+// modifire array
+static uint8_t modArray[3]={0,HID_KEYBOARD_MODIFIER_RIGHTSHIFT,HID_KEYBOARD_MODIFIER_RIGHTALT};
 
 	// string to be sent
-	static uint8_t stringToSend[12] = {
+	static uint8_t stringToSend[5] = {
 		GERMAN_KEYBOARD_SC_F,
 		GERMAN_KEYBOARD_SC_D,
 		GERMAN_KEYBOARD_SC_Y,
-		GERMAN_KEYBOARD_SC_ENTER,
 		GERMAN_KEYBOARD_SC_F,
-		GERMAN_KEYBOARD_SC_D,
-		GERMAN_KEYBOARD_SC_Z,
 		GERMAN_KEYBOARD_SC_ENTER,
-		GERMAN_KEYBOARD_SC_F,
-		GERMAN_KEYBOARD_SC_L,
-		GERMAN_KEYBOARD_SC_D,
-		GERMAN_KEYBOARD_SC_ENTER
 	};
+	stringToSend[0] =	stringAlphabet[IndexAlphabet];
 
-	if(indexToSend < 12) {
+
+	if(indexToSend < 5) {
 		if(characterSent) {
 			report->Modifier = 0;
 			report->Reserved = 0;
@@ -114,21 +218,36 @@ bool CALLBACK_HID_Device_CreateHIDReport(
 	//		timeMeasured[indexToSend]=ticks;
 			++indexToSend;
 		} else {
-			if (indexToSend == 1 || indexToSend == 5){report->Modifier =HID_KEYBOARD_MODIFIER_RIGHTSHIFT;}
-			else {report->Modifier = 0;}//HID_KEYBOARD_MODIFIER_RIGHTALT;// altgr = 6 = 0
-			//report->Modifier = 0;
+			//if (indexToSend == 1 || indexToSend == 5){report->Modifier =HID_KEYBOARD_MODIFIER_RIGHTSHIFT;}
+			//else {report->Modifier = 0;}//HID_KEYBOARD_MODIFIER_RIGHTALT;// altgr = 6 = 0x40
+			report->Modifier = modArray[indexMod];
 			report->Reserved = 0;
 			report->KeyCode[0] = stringToSend[indexToSend];
 			characterSent = 1;
 		//	ticks = 0
 		}
-	} else {
+	} else if(IndexAlphabet <36 && indexMod <2){
 		// indexNumOn  = 0;
 		// indexNumOff = 0;
+
 		indexCapsOn = 0;
 		indexCapsOff= 0;
+		indexToSend = 0;
+		IndexAlphabet++;
 		//printf("%d\n", timeMeasured[0]);
+	} else if (indexMod >= 2) // end of transMission (one char of the password)
+	{
+		IndexAlphabet =0;
+		//indexMod=0;
+		indexToSend =12;
+		FindMax(indexNumOn);
+	} else //if (IndexAlphabet >=49 && indexMod <1)
+	{
+		IndexAlphabet =0;
+		indexToSend = 0;
+		indexMod++;
 	}
+
 
 	return true;
 }
@@ -144,20 +263,20 @@ void CALLBACK_HID_Device_ProcessHIDReport(
 
 	if(*report & HID_KEYBOARD_LED_NUMLOCK)
 		{XMC_GPIO_SetOutputHigh(LED1);
-		timeNumOn[indexNumOn]=ticks;
+		timeNumOn[indexNumOn]=ticks; //end time of string
 		indexNumOn++;}
 	else
 		{XMC_GPIO_SetOutputLow(LED1);
-		timeNumOff[indexNumOff]=ticks;
+		timeNumOff[indexNumOff]=ticks;// starte time of string
 		indexNumOff++;
-		if(indexNumOff == 3)
-			{
-				int loop;
-				for(loop = 0; loop < 3; loop++)
-	      {
-					printf("%d ", timeNumOff[loop]);//printf("%d\n%d\n%d\n", timeNumOff[0], timeNumOff[1], timeNumOff[2]);}
-				}
- 			}
+		// if(indexNumOff == 3)
+		// 	{
+		// 		int loop;
+		// 		for(loop = 0; loop < 3; loop++)
+	  //     {
+		// 			//printf("%d ", timeNumOff[loop]);//printf("%d\n%d\n%d\n", timeNumOff[0], timeNumOff[1], timeNumOff[2]);}
+		// 		}
+ 		// 	}
 		}
 	if(*report & HID_KEYBOARD_LED_CAPSLOCK)
 		{XMC_GPIO_SetOutputHigh(LED2);
@@ -188,4 +307,18 @@ void SysTick_Handler(void)
 {  //static uint32_t ticks = 0;
 
   ticks++;
+}
+
+int FindMax(int count){ // count is the number of elements in timeNumOn or timeCapsOff
+	uint32_t transTime[count]=0;
+	uint8_t LongestTrans=0;
+	for (size_t i = 0; i < count; i++) {
+		transTime[i] = timeNumOff-timeNumOn;
+		if (transTime[i] > LongestTrans) {
+			transTime[i] = LongestTrans;
+		}
+
+	}
+return LongestTrans;
+
 }
